cf35b877f92442b91f5fc5fe1da1ad425cf1fb59
Nicolas Justo <nicolasjnascimento1002@gmail.com>
2026-08-03 18:12:47 -0300

    classe abstrata junto com polimorfismo

---
+++ UdemyPOO/abstração.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+class Notificacao(ABC):
+    def __init__(self, msg) -> None:
+        self.msg = msg
+
+    @abstractmethod
+    def enviar(self) -> bool:
+        ...
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self):
+        print('Email: enviando\n', self.msg)
+        return True
+
+class NotificacaoCorreio(Notificacao):
+    def enviar(self):
+        print('Enviando via SMS:', self.msg)
+        return False
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+
+    if notificacao_enviada:
+        print("Notificação enviada")
+    else:
+        print("Notificação NAO enviada")
+
+email = NotificacaoEmail("ola mundo")
+sms = NotificacaoCorreio("Oi mundo")
+
+email.enviar()
+sms.enviar()
+
+notificar(sms)
